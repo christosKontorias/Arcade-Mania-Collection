19a2bb5351a760722b2b6d5fc0a7e3245092f7c7
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-09-17 15:23:07 +0300

    Improvements

--- app/src/main/java/com/example/arcademania/CreateProfileActivity.java
+++ app/src/main/java/com/example/arcademania/CreateProfileActivity.java
@@ -17,7 +17,6 @@ import com.google.android.material.textfield.TextInputEditText;
 public class CreateProfileActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE_REQUEST = 1;
-
     private TextInputEditText editTextName, editTextSurname, editTextEmail, editTextBirthday, editTextMobile, editTextCountry, editTextPostCode;
     private ImageView uploadImageView;
     private Uri selectedImageUri;
@@ -30,10 +29,13 @@ public class CreateProfileActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_create_profile);
 
+        initializeViews();
+        setupListeners();
+    }
+
+    private void initializeViews() {
         uploadImageView = findViewById(R.id.uploadImage);
         FloatingActionButton fab = findViewById(R.id.floating_action_button);
-        fab.setOnClickListener(view -> openImagePicker());
-
         editTextName = findViewById(R.id.editTextName);
         editTextSurname = findViewById(R.id.editTextSurname);
         editTextEmail = findViewById(R.id.editTextEmail);
@@ -41,15 +43,15 @@ public class CreateProfileActivity extends AppCompatActivity {
         editTextMobile = findViewById(R.id.editTextMobile);
         editTextCountry = findViewById(R.id.editTextCountry);
         editTextPostCode = findViewById(R.id.editTextPostCode);
+        Button submitButton = findViewById(R.id.submitButton);
+    }
+
+    private void setupListeners() {
+        FloatingActionButton fab = findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(view -> openImagePicker());
 
         Button submitButton = findViewById(R.id.submitButton);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                submitForm();
-            }
-        });
-
+        submitButton.setOnClickListener(v -> submitForm());
     }
 
     private void openImagePicker() {
@@ -69,31 +71,58 @@ public class CreateProfileActivity extends AppCompatActivity {
     }
 
     private void submitForm() {
-        // Extract data from EditText fields
-        String name = editTextName.getText().toString();
-        String surname = editTextSurname.getText().toString();
-        String email = editTextEmail.getText().toString();
-        String birthday = editTextBirthday.getText().toString();
-        String mobile = editTextMobile.getText().toString();
-        String country = editTextCountry.getText().toString();
-        String postCode = editTextPostCode.getText().toString();
+        String name = editTextName.getText().toString().trim();
+        String surname = editTextSurname.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String birthday = editTextBirthday.getText().toString().trim();
+        String mobile = editTextMobile.getText().toString().trim();
+        String country = editTextCountry.getText().toString().trim();
+        String postCode = editTextPostCode.getText().toString().trim();
 
-        // Check if any required field is blank
-        if (name.isEmpty() || surname.isEmpty() || email.isEmpty()) {
-            displayMessage("Profile not created!");
+        if (!isValidForm(name, surname, email)) {
+            displayMessage("Please fill in all required fields.");
             return;
         }
 
-        // Save data to SharedPreferences
+        if (!isValidEmail(email)) {
+            displayMessage("Please enter a valid email address.");
+            return;
+        }
+
+        if (!isValidDate(birthday)) {
+            displayMessage("Please enter a valid date (DD/MM/YYYY).");
+            return;
+        }
+
+        if (!isValidMobile(mobile)) {
+            displayMessage("Please enter a valid mobile number (+30-0123456789).");
+            return;
+        }
+
+        if (!isValidPostalCode(postCode)) {
+            displayMessage("Please enter a valid post code (5-digit).");
+            return;
+        }
+
+        saveProfileData(name, surname, email, birthday, mobile, country, postCode);
+        displayMessage("Profile created!");
+        updateFragmentProfile();
+        navigateBackToFragmentProfile();
+    }
+
+    private boolean isValidForm(String name, String surname, String email) {
+        return !name.isEmpty() && !surname.isEmpty() && !email.isEmpty();
+    }
+
+    private void saveProfileData(String name, String surname, String email, String birthday, String mobile, String country, String postCode) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
         if (selectedImageUri != null) {
             editor.putString("imageUri", selectedImageUri.toString());
-            editor.apply();
         }
 
-        editor.putBoolean("accountCreated", true); // Set the flag to true
+        editor.putBoolean("accountCreated", true);
         editor.putString("name", name);
         editor.putString("surname", surname);
         editor.putString("email", email);
@@ -102,16 +131,6 @@ public class CreateProfileActivity extends AppCompatActivity {
         editor.putString("country", country);
         editor.putString("postCode", postCode);
         editor.apply();
-
-        displayMessage("Profile created!");
-        updateFragmentProfile();
-        navigateBackToFragmentProfile();
-
-        // Set a result indicating that the profile has been created
-        Intent resultIntent = new Intent();
-        resultIntent.putExtra("profileCreated", true);
-        setResult(RESULT_OK, resultIntent);
-        finish();
     }
 
     private void updateFragmentProfile() {
@@ -127,5 +146,24 @@ public class CreateProfileActivity extends AppCompatActivity {
 
     private void navigateBackToFragmentProfile() {
         super.onBackPressed();
+    }
+
+    private boolean isValidEmail(String email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean isValidDate(String date) {
+        String regexPattern = "\\d{2}/\\d{2}/\\d{4}";
+        return date.matches(regexPattern);
+    }
+
+    private boolean isValidMobile(String mobile) {
+        String regexPattern1 = "^\\+[0-9]{1,3}-[0-9]{10}$";
+        return mobile.matches(regexPattern1);
+    }
+
+    private boolean isValidPostalCode(String postalCode) {
+        String regexPattern = "^[0-9]{5}$";
+        return postalCode.matches(regexPattern);
     }
 }
